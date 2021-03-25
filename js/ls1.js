26405c4b2c15425dad88f70b61ea6a37179818ad
BoikoDenys <dinisboyko1337@gmail.com>
2021-03-25 21:10:28 +0200

    js lesson 1

--- js/ls1.js
+++ js/ls1.js
@@ -1,33 +1,189 @@
 // Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 //  Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+// let hello = 'hello'
+// let owu = 'owu'
+// let com = 'com'
+// let ua = 'ua'
+// let one = '1'
+// let ten = '10'
+// let numberNine = '-999'
+// let numberOne = '123'
+// let pi = '3.14'
+// let numberTwo = '2.7'
+// let numberThre = '16'
+// let bulOne = 'true'
+// let bulTwo = 'false'
+//
+// console.log(hello)
+// console.log(owu)
+// console.log(com)
+// console.log(ua)
+// console.log(one)
+// console.log(ten)
+// console.log(numberNine)
+// console.log(numberOne)
+// console.log(pi)
+// console.log(numberTwo)
+// console.log(numberThre)
+// console.log(bulOne)
+// console.log(bulTwo)
+// alert(hello)
+// alert(owu)
+// alert(com)
+// alert(ua)
+// alert(one)
+// alert(ten)
+// alert(numberNine)
+// alert(numberOne)
+// alert(numberTwo)
+// alert(numberThre)
+// alert(bulOne)
+// alert(bulOne)
+// document.write(hello)
+// document.write(owu)
+// document.write(com)
+// document.write(ua)
+// document.write(one)
+// document.write(ten)
+// document.write(numberNine)
+// document.write(numberOne)
+// document.write(numberTwo)
+// document.write(numberThre)
+// document.write(bulOne)
+// document.write(bulTwo)
 
-let hello = 'hello'
-let owu = 'owu'
-let com = 'com'
-let ua = 'ua'
-let one = '1'
-let ten = '10'
-let numberNine = '-999'
-let numberOne = '123'
-let pi = '3.14'
-let numberTwo = '2.7'
-let numberThre = '16'
-let bulOne = 'true'
-let bulTwo = 'false'
+// Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//  Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+// hello = 'bue'
+// owu = 'uwo'
+// com = 'bip'
+// ua = 'uu'
+// one = '2'
+// ten = '180'
+// numberNine = '-999'
+// numberOne = '145'
+// pi = 'pi'
+// numberTwo = '2i7'
+// numberThre = 'sixtene'
+// bulOne = '1'
+// bulTwo = 'true'
+//
+// console.log(hello)
+// console.log(owu)
+// console.log(com)
+// console.log(ua)
+// console.log(one)
+// console.log(ten)
+// console.log(numberNine)
+// console.log(numberOne)
+// console.log(pi)
+// console.log(numberTwo)
+// console.log(numberThre)
+// console.log(bulOne)
+// console.log(bulTwo)
+// alert(hello)
+// alert(owu)
+// alert(com)
+// alert(ua)
+// alert(one)
+// alert(ten)
+// alert(numberNine)
+// alert(numberOne)
+// alert(numberTwo)
+// alert(numberThre)
+// alert(bulOne)
+// alert(bulOne)
+// document.write(hello)
+// document.write(owu)
+// document.write(com)
+// document.write(ua)
+// document.write(one)
+// document.write(ten)
+// document.write(numberNine)
+// document.write(numberOne)
+// document.write(numberTwo)
+// document.write(numberThre)
+// document.write(bulOne)
 
-console.log(hello)
-console.log(owu)
-console.log(com)
-console.log(ua)
-console.log(one)
-console.log(ten)
-console.log(numberNine)
-console.log(numberOne)
-console.log(pi)
-console.log(numberTwo)
-console.log(numberThre)
-console.log(bulOne)
-console.log(bulTwo)
+// За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//  Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+//
+// const userName = prompt('What is your name')
+// const userSur = prompt('What is your surname')
+// const userAge = prompt('How old are you')
+//
+// document.write("Вітаю ")
+// document.write(userName)
+// document.write(" ")
+// document.write(userSur)
+// document.write('.Вам ')
+// document.write(userAge)
+// document.write(" років")
 
+// Взяти змінні з завдання вище та зєднати їх в одну змінну (конкатинація)
+// document.write(userName + userSur + userAge)
 
+// За допомогою prompt() отримати 3 числа. Привести їх до числа (Google "String to number JS"). Вивести їх в консоль.
+// let userId1 = prompt()
+// let userId2 = prompt()
+// let userId3 = prompt()
+// console.log(userId1 * 1)
+// console.log(userId2 * 1)
+// console.log(userId3 * 1)
 
+// За допомогою prompt() отримати 4 числа. Привести їх до числа за допомогою paeseInt.
+// Додати їх всі мож собою і результат вивести в консоль.
+//
+// let userId1 = parseInt(prompt())
+// let userId2 = parseInt(prompt())
+// let userId3 = parseInt(prompt())
+// let userId4 = parseInt(prompt())
+// let sum = userId1+userId2+userId3+userId4
+// console.log(sum)
+
+// За допомогою prompt() отримати 3 числа з плаваючою точкою за допомогою parseFloat
+//
+// let userId2 = parseFloat(prompt())
+// let userId3 = parseFloat(prompt())
+// let userId4 = parseFloat(prompt())
+
+// За допомогою prompt() отримати 3 числа з плаваючою точкою. Заокруглити їх (Google "round number JS"). Просумувати їх між собою. Результат вивести в консоль.
+//
+// let userId1 = parseFloat(prompt())
+// let userId2 = parseFloat(prompt())
+// let userId3 = parseFloat(prompt())
+//
+// console.log(Math.round(userId1+userId2+userId3))
+
+// За допомогою prompt() отримати 2 цілих числа.
+//   Перше число - це число яке будуть підносити до степеня. Друге число - це число яке являється степенню.
+//   За допомогою Math.pow (Google) піднести перше число в степінь другого числа.
+//   Example. Число 1 = 5. Число 2 = 2. Результат = 25
+//
+//  let a = parseInt(prompt())
+//  let b = parseInt(prompt())
+// console.log(Math.pow(a, b))
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+//
+// let a = 100
+// let b = '100'
+// let c = true
+// console.log(typeof a,b,c)
+
+// Поставет відповідний оператор в виразах що б вийшов відповідний результат.
+//   В однакових виразаї не використовувати однакові оператори!!!
+//     5 < 6 -> true
+//     5 > 6 -> false
+//     5 >= 6 -> false
+//     5 == 6 -> false
+//     10 == 10 -> true
+//     10 === 10 -> true
+//     10 != 10 -> false
+//     10 < 10 -> false
+//     10 > 10 -> false
+//     123 === '123' -> false
+//     123 == '123' -> true
