36fb08940a909743a454bbdbb6902951ada0781e
BoikoDenys <dinisboyko1337@gmail.com>
2021-03-25 18:11:50 +0200

    js lesson 1

---
+++ js/ls1.js
@@ -0,0 +1,33 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//  Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+let hello = 'hello'
+let owu = 'owu'
+let com = 'com'
+let ua = 'ua'
+let one = '1'
+let ten = '10'
+let numberNine = '-999'
+let numberOne = '123'
+let pi = '3.14'
+let numberTwo = '2.7'
+let numberThre = '16'
+let bulOne = 'true'
+let bulTwo = 'false'
+
+console.log(hello)
+console.log(owu)
+console.log(com)
+console.log(ua)
+console.log(one)
+console.log(ten)
+console.log(numberNine)
+console.log(numberOne)
+console.log(pi)
+console.log(numberTwo)
+console.log(numberThre)
+console.log(bulOne)
+console.log(bulTwo)
+
+
+
